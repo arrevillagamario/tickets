53513454d6168e9df33c8f94c8e2a1ff96ceca34
arrevillagamario <marrevillaga03@gmail.com>
2024-05-25 18:56:04 -0600

    Diseño del programa

---
+++ tickets/Controllers/TicketsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tickets.Controllers
+{
+    public class TicketsController : Controller
+    {
+        // GET: TicketsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CrearTicket()
+        {
+            return View();
+        }
+
+        public ActionResult TicketsCreados()
+        {
+            return View();
+        }
+        public ActionResult TicketsEnProgreso()
+        {
+            return View();
+        }
+        public ActionResult TicketsEnPausa()
+        {
+            return View();
+        }
+        public ActionResult TicketsResueltos()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ tickets/Controllers/UsuariosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tickets.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: UsuariosController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Interno()
+        {
+            return View();
+        }
+        public ActionResult Externo()
+        {
+            return View();
+        }
+
+
+    }
+}
